9939e919bed4c8bb92b13cc53390bed80f877014
phhai <phhai@ymail.com>
2023-10-17 09:43:16 +0700

    enhance feature 1

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Hello World'),
+      home: const MyHomePage(title: 'Hello CQ2021'),
     );
   }
 }
