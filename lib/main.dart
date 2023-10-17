b9981a69b49b5d64077b8eff887a853280ea08ae
phhai <phhai@ymail.com>
2023-10-17 10:03:15 +0700

    worked on feature 3

--- lib/main.dart
+++ lib/main.dart
@@ -57,6 +57,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  // Update feature 3
   void _incrementCounter() {
     print('Hello CQ2021');
     setState(() {
